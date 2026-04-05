684242cfb1a6a469d35780f22d8873f43eb81d78
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 17:37:05 -0700

    feat: add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
+  },
+  experimental: {
+    serverActions: { allowedOrigins: ["localhost:3000"] },
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+});
